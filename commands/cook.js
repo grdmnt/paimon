5b9973122abaa352ec0ffd0c15d40284fcf6e7c1
Cjay Billones <billoensciprian08@gmail.com>
2020-10-22 19:51:07 +0800

    Refactor to use advanced command handler

---
+++ commands/cook.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'cook',
+    description: 'This is for cooking Paimon.',
+    execute(message, args){
+      if(args.length === 0){
+        message.channel.send('Would you like to cook some Sticky Honey Roast? :drool:');
+      } else if(args.length === 1 && args[0] === "Paimon"){
+        message.channel.send('No! Paimon is NOT EMERGENCY FOOD! :face_with_symbols_over_mouth:');
+      }
+                 
+    }
+}
--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
-bot.login(TOKEN);
+const fs = require('fs');
+
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+	const command = require(`./commands/${file}`);
+	bot.commands.set(command.name, command);
+}
 
 bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
+	console.info(`Logged in as ${bot.user.tag}!`);
 });
+
+const prefix = "!";
+
+bot.on("message", message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if(command === "cook"){
+    bot.commands.get('cook').execute(message, args);
+	}
+});
+
+bot.login(TOKEN);
