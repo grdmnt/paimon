b5fadd54899033492e936b8299ddea2442d0781b
Cjay Billones <billoensciprian08@gmail.com>
2020-10-24 17:01:29 +0800

    Add wiki command
    This accepts the "wiki" command. If there are no input arguments, it will send a link to the Genshin Impact wiki fandom. If there are input arguments, it will send the wiki page of the specific search input.

---
+++ commands/wiki.js
@@ -0,0 +1,19 @@
+const { execute } = require("./domain");
+
+function capitalize(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports = {
+  name: "wiki",
+  description: "Returns the homepage of the Genshin Impact wiki fandom.",
+  execute(message, args){
+    if(args.length === 0){
+      message.channel.send("https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki");
+    } else if(args.length === 1){
+      message.channel.send(`https://genshin-impact.fandom.com/wiki/${capitalize(args[0].toLowerCase())}`);
+    } else {
+      message.channel.send(`https://genshin-impact.fandom.com/wiki/${args.map(string => (capitalize(string))).join("_")}`);
+    }
+  }
+}
