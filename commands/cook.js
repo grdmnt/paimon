e589286bdf3a8033168be432879d4d39fbfdb12e
Cjay Billones <billonesciprian08@gmail.com>
2020-10-24 19:06:55 +0800

    Ignore case sensitivity for cook Paimon command

--- commands/cook.js
+++ commands/cook.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args){
       if(args.length === 0){
         message.channel.send('Would you like to cook some Sticky Honey Roast? :drool:');
-      } else if(args.length === 1 && args[0] === "Paimon"){
+      } else if(args.length === 1 && args[0].toLowerCase() === "paimon"){
         message.channel.send('No! Paimon is NOT EMERGENCY FOOD! :face_with_symbols_over_mouth:');
       }
                  
