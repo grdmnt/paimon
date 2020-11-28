5ed74ef73b99c25fd04f629b6a9bf7b049c885fd
Cjay Billones <billonesciprian08@gmail.com>
2020-11-28 20:22:33 +0800

    Add level command
    This command allows the bot to send images from imgur that contains the summaries of materials and costs for levelling up characters, weapons, and talents.

---
+++ commands/level.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "level",
+  description: "Returns a chart of character and weapon level cost summaries.",
+  execute(message, args){
+    if(args[0].toLowerCase() === "character"){
+      message.channel.send("https://i.imgur.com/15D6cor.png");
+    } else if(args[0].toLowerCase() === "weapon"){
+      message.channel.send("https://imgur.com/ko1zwgo");
+    } else if(args[0].toLowerCase() === "talent"){
+      message.channel.send("https://imgur.com/PZdMbcT");
+    } else{
+      sendHelpMessage(message);
+    }
+  }
+}
+
+function sendHelpMessage(message){
+  var embed = new Discord.MessageEmbed()
+                         .setColor("#FF0000")
+  embed.addFields(
+    {
+      name: "Command Usage: ",
+      value: "?level [option]\n\n Bot will accepts `character` and `weapon` as valid options and will return an image containing the summary of weapon and character levels and their corresponding costs."
+    }
+  )
+  message.channel.send(embed);
+}
