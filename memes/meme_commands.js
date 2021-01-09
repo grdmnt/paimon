f7a8f569f4d77bf9a227447bb1cc838199903f82
agjuarez <agjuarez@users.noreply.github.com>
2021-01-10 03:13:58 +0800

    Update zhongli command
    updated zhongli command with  another image and made it random

--- memes/meme_commands.js
+++ memes/meme_commands.js
@@ -18,9 +18,9 @@ module.exports = {
       name: "zhongli",
       description: "A bunch of meme commands",
       execute(message, _) {
-        const broke = new Discord.MessageEmbed().setImage(
-          "https://media1.tenor.com/images/373c80cfb4498d2b043572ef58ceb516/tenor.gif"
-        );
+        let images = ["https://media1.tenor.com/images/373c80cfb4498d2b043572ef58ceb516/tenor.gif", "https://i.imgur.com/1EON1kN.png"];
+        const image = images[getRandomInt(images.length)];
+        const broke = new Discord.MessageEmbed().setImage(image);
         message.channel.send(broke);
       },
     },
